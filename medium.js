1b6095a9dc727e0d1801e32751d666d4121ecb70
Matt Sayler <matt.sayler+code@gmail.com>
2015-08-04 16:13:13 -0700

    Update medium.js

--- medium.js
+++ medium.js
@@ -9,11 +9,13 @@
         marked.setOptions({color: true})
         var md = document.getElementById("md");
         var html = marked(pres);
-        console.log(html);
-        var startTag = new RegExp("\<(p|h\d|pre|ul)((?:\s*)?(?:id|class)\=[\'\"]\w+?[\'\"](?:\s*)?)?\>", "g");
+        var startTag = new RegExp("\<(p|h|pre|ul)\d?\>", "g");
+        // ((?:\s*)?(?:id|class)\=[\'\"]\w+?[\'\"](?:\s*)?)?
         var endTag = new RegExp("\<\/(p|h\d|pre|ul)\>", "g");
-        md.innerHTML = html.replace(startTag, function(match) {console.log(match); var txt = '<div>' + match; console.log(txt); return txt;});
-        md.innerHTML = html.replace(endTag, function(match) {console.log(match); var txt = match + '</div>'; console.log(txt); return txt;});
+        html.replace(startTag, function(match) {console.log(match); var txt = '<div>' + match; console.log(txt); return txt;});
+        html.replace(endTag, function(match) {console.log(match); var txt = match + '</div>'; console.log(txt); return txt;});
+        console.log(html);
+        md.innerHTML = html;
         var s = document.getElementsByTagName('div'), cur = 0;
         if (!s) return;
         function go(n) {
