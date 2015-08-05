c318b06a46f802204460b6875f48edbfee2b844f
Matt Sayler <matt.sayler+code@gmail.com>
2015-08-05 09:47:29 -0700

    Update medium.js

--- medium.js
+++ medium.js
@@ -11,8 +11,10 @@
         var html = marked(pres);
         var startTag = new RegExp("\<(p|h|pre|ul)[0-9]?((?:\s*)?(?:id|class)\=[\'\"]\w+?[\'\"](?:\s*)?)?\>", "g");
         var endTag = new RegExp("\<\/(p|h|pre|ul)[0-9]?\>", "g");
-        html = html.replace(startTag, function(match) {console.log(match); var txt = '<div>' + match; console.log(txt); return txt;});
-        html = html.replace(endTag, function(match) {console.log(match); var txt = match + '</div>'; console.log(txt); return txt;});
+        var fixImg = new RegExp("(\<[p]\s?.+\>)(?:\<[img]\s.+\s?\>)?(\<\/[p]\>)", "g")
+        html = html.replace(startTag, function(match) {var txt = '<div>' + match; return txt;});
+        html = html.replace(endTag, function(match) {var txt = match + '</div>'; return txt;});
+        html = html.replace(fixImg, function(match) {conslole.log(match); var txt = ''; console.log(txt); return txt;});
         console.log(html);
         md.innerHTML = html;
         var s = document.getElementsByTagName('div'), cur = 0;
